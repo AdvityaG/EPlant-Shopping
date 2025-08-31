9e93a61423bb1d232e161d134dc43e62f545ad9c
advitya <advityagarg1@gmail.com>
2025-08-31 14:41:43 -0500

    added code to Cart Item

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
+import { isRejected } from '@reduxjs/toolkit';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
@@ -9,27 +10,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total= 0;
+    cart.forEach((item) => {
+      total += item.quantity * parseFloat(item.cost.substring(1));
+    })
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    alert("Functionality to be added for future refernce");
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity>1){
+      dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    }
+    else{
+      dispatch(removeItem({name: item.name}));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({name: item.name}));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let total= item.quantity * parseFloat(item.cost.substring(1));
+    
   };
 
   return (
